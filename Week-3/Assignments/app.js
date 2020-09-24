ba169887f1264cd55fc3f635b53512461802d300
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 13:18:43 +0800

    add cookie parser as well

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser= require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
