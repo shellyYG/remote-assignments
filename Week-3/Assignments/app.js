ed946b4030ddbf8860b294ecfbe56ac3bff3a5a5
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 13:16:44 +0800

    create basic folder strucuture

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const bodyParser= require('body-parser');
+
+app.set('view engine', 'pug');
+
+
 const app = express();
 app.get('/', (req, res) => {
     res.send("Hey! this is Shelly's first hand-made server!");
