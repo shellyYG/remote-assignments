8df64ca5b018b77661c1ca3e6a050cd38bb117ed
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 23:42:50 +0800

    add dynamic URL

--- Week-3/Assignments/public/interactive.js
+++ Week-3/Assignments/public/interactive.js
@@ -1,6 +1,25 @@
 let button = document.querySelector("#btn");
+let ss = 3;
 
 button.addEventListener('click', ()=> {
-    let ss = document.querySelector('#addinput').value;
-    alert(ss);
+    ss = document.querySelector('#addinput').value;
+    ss = parseInt(ss);
 });
+
+
+
+
+let xhr = new XMLHttpRequest();
+        
+xhr.responseType = 'json';//so we don't need to parse JSON later
+
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+        const data = xhr.response;
+        console.log(data);
+    }
+};
+
+xhr.open('GET','http://localhost:3000/getData?number='+ss);
+
+xhr.send();
