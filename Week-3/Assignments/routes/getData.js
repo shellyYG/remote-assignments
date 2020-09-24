44b3f3ad737821bd556f6f853347c886fccfa591
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 14:27:49 +0800

    add logic for counting

--- Week-3/Assignments/routes/getData.js
+++ Week-3/Assignments/routes/getData.js
@@ -6,4 +6,25 @@ router.get('/', (req, res) => {
     res.send("Lack of Parameter");
 });
 
+router.get('/:id', (req, res) => {
+    const number = req.query.side;
+    if (!number) {
+        return res.redirect(`/`);
+    } else if (typeof(number) == "number") {
+        const templateData = { };
+        nn = 0;
+        for (i=0; i<number; i++) {
+            nn +=i;
+        }
+        templateData.number = nn;
+    } else {
+        const templateData = {};
+        nn = 'Wrong Parameter';
+    }
+    res.render('getData', templateData);
+});
+
+
+
+
 module.exports = router;
