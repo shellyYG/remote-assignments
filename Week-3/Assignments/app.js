704a3f5846571286650f2b785fdbbf66b8593709
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 13:47:36 +0800

    add a console.log for when starting the server

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -12,9 +12,9 @@ const mainRoutes = require('./routes');
 const getDataRoutes = require('./routes/getData')
 
 
-app.use(mainRoutes);
-app.use('/getData', getDataRoutes);
+//app.use(mainRoutes);
+//app.use('/getData', getDataRoutes);
 
-app.listen(3000, ()=> {	
-        console.log('The application is running on localhost:3000! by Shelly the smart girl')
-});
+app.listen(3000, () => {	
+	console.log('The application is running on localhost:3000! by Shelly the smart girl')
+});	
