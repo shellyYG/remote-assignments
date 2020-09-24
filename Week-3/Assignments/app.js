adf48ff7c83b0b101c0d59e5e36b5bf60ca3d420
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 11:00:12 +0800

    change website text

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hey! this is a server!');
+    res.send("Hey! this is Shelly's first hand-made server!");
 });
 
 app.listen(3000);
