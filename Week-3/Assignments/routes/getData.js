eaffb9a1b1afe55386c20371a9c268dd5f245b15
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-24 17:24:06 +0800

    dealt with type of number

--- Week-3/Assignments/routes/getData.js
+++ Week-3/Assignments/routes/getData.js
@@ -2,32 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 
+//router.get('/', (req, res) => {
+//    res.send("Lack of Parameter");
+//});
+
 router.get('/', (req, res) => {
-    res.send("Lack of Parameter");
-});
-
-router.get('/:id', (req, res) => {
-    let number = req.query.number;
-    const { id } = req.params;
-
-    if (!number) { //if there is no side query, go back to localhost:3000
-        return res.redirect(`/`);
-    } else if (typeof(number) == "number") {
-        const templateData = { };
-        nn = 0;
-        for (i=0; i<number; i++) {
-            nn +=i;
-        }
-        templateData.number = nn;
+    let nn = req.query.number;
+    if (!nn) { //means did not have ?number in the URL
+        res.send("Lack of Parameter");
     } else {
-        const templateData = { };
-        nn = 'Wrong Parameter';
-        templateData.number = nn;
+        nn = parseInt(nn);
+        if (Number.isInteger(nn)) { //if its an interger
+            let x = 0;
+            for (i=0; i<=nn; i++) {
+                x += i;
+            }
+            if (x<=0) {
+                x="Wrong Parameter";
+            } else {
+                x;
+            }
+            res.json(x);
+        } else {
+            let x='Wrong Parameter';
+            res.json(x);
+        }
+
     }
-    res.render('getData', templateData);
 });
-
-
-
 
 module.exports = router;
