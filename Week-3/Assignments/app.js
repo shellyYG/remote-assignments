aff87bc90a02861e82aa10ea7a5114b68b1bc349
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-25 12:33:38 +0800

    add trackName routes and connect to app.js

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -10,10 +10,12 @@ app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes/index.js');
 const getDataRoutes = require('./routes/getData');
+const trackNameRoutes = require('./routes/trackName');
 
 
 app.use(mainRoutes);
 app.use('/getData', getDataRoutes);
+app.use('/trackName', trackNameRoutes);
 app.use(express.static('public'));
 
 app.post('/trackName', (req, res) => {
---
+++ Week-3/Assignments/routes/trackName.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => { //when customer arrive trackName...what will happen?
+//Code Blocker
+});
+
+
+module.exports = router;
