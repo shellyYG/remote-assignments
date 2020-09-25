314f7abf52fbf94ed8eb071b0103b9e99696de68
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-25 16:37:26 +0800

    from trackName redirect back to myName

--- Week-3/Assignments/routes/trackName.js
+++ Week-3/Assignments/routes/trackName.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => { //when customer arrive trackName...what will happen?
     req.query.name = req.cookies.cookiect;
-    //console.log(req.query.name); //successfully print out Shelli
-    res.render('trackName',{name: req.cookies.cookiect});
+    console.log(req.query.name); //successfully print out Shelli
+    //res.render('trackName',{name: req.cookies.cookiect});
+    res.redirect('/myName');
 });
 
 
