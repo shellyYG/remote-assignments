3b8feaea43884ebd2955e073af5cbf4b6f7c9e42
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-25 09:41:45 +0800

    wrap all the things inside eventlistener

--- Week-3/Assignments/public/interactive.js
+++ Week-3/Assignments/public/interactive.js
@@ -4,20 +4,21 @@ var ss =10;
 button.addEventListener('click', ()=> {
     ss = document.querySelector('#addinput').value;
     ss = parseInt(ss);
+
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';//so we don't need to parse JSON later
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            const data = xhr.response;
+            console.log(data);
+        }
+    };
+
+    xhr.open('GET','http://localhost:3000/getData?number='+ss);
+
+    xhr.send();
 });
+//Why is ss still 10, not changed by the EventListener? QQ
 
 
-
-let xhr = new XMLHttpRequest();
-xhr.responseType = 'json';//so we don't need to parse JSON later
-
-xhr.onreadystatechange = function() {
-    if(xhr.readyState === 4) {
-        const data = xhr.response;
-        console.log(data);
-    }
-};
-
-xhr.open('GET','http://localhost:3000/getData?number='+ss);
-
-xhr.send();
