fe6c0d1f8d9b2b0bd14bf465a2d207b3e6409f4f
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-25 09:54:54 +0800

    add console log to check execution sequence

--- Week-3/Assignments/public/interactive.js
+++ Week-3/Assignments/public/interactive.js
@@ -2,8 +2,10 @@ let button = document.querySelector("#btn");
 var ss =10;
 
 button.addEventListener('click', ()=> {
+    //1. console.log("hi beginner") 
     ss = document.querySelector('#addinput').value;
     ss = parseInt(ss);
+    //2. console.log(typeof ss)
 
     let xhr = new XMLHttpRequest();
     xhr.responseType = 'json';//so we don't need to parse JSON later
@@ -11,14 +13,16 @@ button.addEventListener('click', ()=> {
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4) {
             const data = xhr.response;
-            console.log(data);
+            console.log(data); //6.
+            //7. console.log("Inside xhr after data");
         }
     };
-
+    //3. console.log(ss)
     xhr.open('GET','http://localhost:3000/getData?number='+ss);
-
+    //4. console.log("after open");
     xhr.send();
+    //5. console.log("after send");
 });
-//Why is ss still 10, not changed by the EventListener? QQ
+
 
 
