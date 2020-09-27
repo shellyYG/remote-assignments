4f8a7e80c7287b2fac8d69a717c930f0e84d8f20
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-27 20:23:28 +0800

    write first thought

---
+++ Week-3/Assignments/assignment5_algo.js
@@ -0,0 +1,17 @@
+function twoSum(nums, target) {
+    for (i=0; i< length; i++) {
+        let firstn = i;
+        for (j = 0; j < (length-firstn); j++) {
+            let secondn = firstn+j;
+        }
+    }
+    console.log("First number is: "+nums[i]);
+    console.log("Second number is: "+nums[j]);
+    let output = nums[i]+nums[j];
+    return output;
+}
+/*
+    For example:
+    twoSum([2, 7, 11, 15], 9); Should returns:
+    [0, 1] Because:
+*/
