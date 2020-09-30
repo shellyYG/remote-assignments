e32135e24b345d3d4fdc26cec270baeaff9d2590
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-30 22:26:41 +0800

    finish week4 assignment1

--- Week-4/Assignment-1/assignment-1.js
+++ Week-4/Assignment-1/assignment-1.js
@@ -1,16 +1,15 @@
 function delayedResult(n1, n2, delayTime, callback) {
     setTimeout(function() {
-        //console.log("I am here");
         x = n1+n2;
-        //console.log("Hi");
         callback(x); //callback in callback
-        //console.log("there");
     }, delayTime);
     
 }
 
+console.log("Below are results!");
+
 delayedResult(4, 5, 3000, function(result) { console.log(result);
 }); // 9 (4+5) will be shown in the console after 3 seconds
 
-delayedResult(-5, 10, 2000, function(result) { console.log(result);
+delayedResult(-5, 10, 10000, function(result) { console.log(result);
 }); // 5 (-5+10) will be shown in the console after 2 seconds
