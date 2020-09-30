444b5dcda287014dcce34943a0ef19b91ae20177
shellyyangtw <chih-ching.yang@whu.edu>
2020-09-30 22:10:52 +0800

    have callback executed but callback is called

--- Week-4/Assignment-1/assignment-1.js
+++ Week-4/Assignment-1/assignment-1.js
@@ -1,5 +1,11 @@
 function delayedResult(n1, n2, delayTime, callback) {
-    setTimeout(callback(n1+n2), delayTime);
+    setTimeout(function() {
+        x = n1+n2;
+        //console.log("Hi");
+        callback(x); //callback in callback
+        //console.log("there");
+    }, delayTime);
+    
 }
 
 delayedResult(4, 5, 3000, function(result) { console.log(result);
