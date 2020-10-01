79ddb6a1a4991932583a1f88e58493adee81c735
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-02 00:51:26 +0800

    use DOM

--- Week-4/Assignment-2/ajaxla.js
+++ Week-4/Assignment-2/ajaxla.js
@@ -3,7 +3,6 @@ function ajax(src, callback){
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4){
             console.log("/1 Shelly log");
-            console.log(xhr.responseText);
             callback(xhr.responseText); //callback == render // data == xhr.response
         }
     }
@@ -12,7 +11,18 @@ function ajax(src, callback){
 }
 
 function render(data){  
-    outputhere.innerHTML = data;    //replace outputhere id section with data               
+    const transformeddata = JSON.parse(data); //change JSON to real JS objects
+    console.log(transformeddata);
+    let ull = document.createElement('ul');
+    
+    transformeddata.forEach(function (tt) {
+        var li = document.createElement('li');
+        li.textContent = tt["name"]+": price "+tt["price"]+"/ 賣點："+tt["description"];
+        ull.appendChild(li);
+    });
+    var output = document.querySelector('#outputhere');
+    output.appendChild(ull);
+    //outputhere.innerHTML = transformeddata[i]["name"];    //replace outputhere id section with data               
 }
 
 let src = "https://cwpeng.github.io/live-records-samples/data/products.json"
