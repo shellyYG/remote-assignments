759701c7a703d6afbd3c82b8eb0a7ed482109f89
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-01 13:57:45 +0800

    building blocks

--- Week-4/hwtwo.js
+++ Week-4/hwtwo.js
@@ -3,29 +3,33 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-app.get('/hw2', (req,res) => {
-    console.log("Hi!! Shelly on the go!");
-    res.render('hw2');
-})
-
-
+// app.get('/hw2', (req,res) => {
+//     console.log("Hi!! Shelly on the go!");
+//     res.render('hw2');
+// })
 // function ajax(src, callback){
 //     callback();
 // }
 
-// function render(data){
-//     addItemButton.addEventListener('click', () => {
-//         let li = document.createElement('li');		
-//         let ul = document.getElementsByTagName('ul')[0];		
-//         li.textContent = addItemInput.value; 		
-//         ul.appendChild(li); //Here you insert the element to DOM!		
-//         addItemInput.value = ''; //this clears the input after the input is submitted		
-//     });			
-// }
+function render(){
+    app.get('/hw2', (req,res) => {
+        console.log("Hi!! Shelly on the go!");
+        res.render('hw2');
+    })
+    // let welcomemsg = document.querySelector("title.intro");
+    // welcomemsg.addEventListener('click', () => {
+    //     welcomemsg.textContent = 'Have a Good Time!';
+    // });			
+}
 
-// ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ render(response);
+render();
+
+app.get('/test', render);
+
+
+//ajax("/hw2", render);//function(response){ render(response);
     
-// }); // you should get product information in JSON format and render data in the page
+//}); // you should get product information in JSON format and render data in the page
 
 app.listen(4000, () => {	
 	console.log('The application is running on localhost:4000!')
