c382eaccf126e3dbc96552afd095356b515c3260
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-01 16:50:37 +0800

    npm install the ajax

--- Week-4/hwtwo.js
+++ Week-4/hwtwo.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 app.set('view engine', 'pug');
 
 function ajax(src, callback){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4){
+        if(xhr.readyState === 4 && xhr.state ===200){
             let restext = JSON.parse(xhr.responseText);
-            callback(restext);
+            callback(restext); //ajax calls the callback and callback use the restext  
             console.log("xhr request is ready!");
         }
         console.log("After xhr onreadystate!")
     }
-    xhr.open('GET',src);
+    xhr.open('GET',src,true);
     xhr.send();
     console.log("After xhr.send!")
 }
@@ -21,13 +22,12 @@ function ajax(src, callback){
 let src = "https://cwpeng.github.io/live-records-samples/data/products.json";
 
 function render(data){
-    app.get('data', (req,res) => {
-        console.log("Hi!! Shelly on the go!");
-        res.render('hw2');
-    })
+    app.render(data);
 }
 
-ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ render(response); });
+ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
+    render(response); 
+});
 
 
 // app.listen(4000, () => {	
