10fdc844e963273c451011eeae9ea9152d28fa1a
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-01 22:23:25 +0800

    finish assignment 2

--- Week-4/Assignment-2/ajaxla.js
+++ Week-4/Assignment-2/ajaxla.js
@@ -1,50 +1,21 @@
-let button = document.querySelector("#btn");
-
 function ajax(src, callback){
-    let xhr = new XHLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
+    var xhr=new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
         if(xhr.readyState === 4){
-            const data = xhr.response;
-            callback(data)
+            callback(xhr.response);
         }
-        xhr.open('GET',src);
-        xhr.send();
     }
+    xhr.open('GET',src);
+    xhr.send();
 }
 
-function render(data){
-    let returncontent = document.createElement("rr");
-    let targetplace = document.querySelector('#outhere');
-    targetplace.innerHTML= data;
-    document.body.appendChild(targetplace);
-    //use document.createElement() and appendChild()
+function render(data){  
+    outputhere.innerHTML = data;    //replace outputhere id section with data               
 }
 
+let src = "https://cwpeng.github.io/live-records-samples/data/products.json"
 
-ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){
+ajax(src, function(response){
     render(response);
 });
 
-
-
-button.addEventListener('click', ()=> {
-    
-    ss = document.querySelector('#addinput').value; //1.
-    ss = parseInt(ss); //2.
-    let xhr = new XMLHttpRequest(); //3.
-    
-    xhr.responseType = 'json';//4. //so we don't need to parse JSON later
-    
-    xhr.onreadystatechange = function() {
-        //5. //8.
-        if(xhr.readyState === 4) {
-            const data = xhr.response; //9.
-            console.log(data); //10.
-        }
-    };
-    
-    xhr.open('GET','http://localhost:3000/getData?number='+ss); //6.
-    xhr.send(); //7.
-   
-});
