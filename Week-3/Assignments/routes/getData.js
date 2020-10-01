45f20f508c751247918e4970ff35be54c187f3a7
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-01 12:52:10 +0800

    clean comments on previous assignments

--- Week-3/Assignments/routes/getData.js
+++ Week-3/Assignments/routes/getData.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-//router.get('/', (req, res) => {
-//    res.send("Lack of Parameter");
-//});
-
 router.get('/', (req, res) => {
     let nn = req.query.number;
     if (!nn) { //means did not have ?number in the URL
