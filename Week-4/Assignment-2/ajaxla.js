cc737ebd8075c2dd4ccfcaa4263c0d25c78fc9a0
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-01 21:22:43 +0800

    put all docs into assignment-2

---
+++ Week-4/Assignment-2/ajaxla.js
@@ -0,0 +1,23 @@
+let button = document.querySelector("#btn");
+var ss =10;
+
+button.addEventListener('click', ()=> {
+    
+    ss = document.querySelector('#addinput').value; //1.
+    ss = parseInt(ss); //2.
+    let xhr = new XMLHttpRequest(); //3.
+    
+    xhr.responseType = 'json';//4. //so we don't need to parse JSON later
+    
+    xhr.onreadystatechange = function() {
+        //5. //8.
+        if(xhr.readyState === 4) {
+            const data = xhr.response; //9.
+            console.log(data); //10.
+        }
+    };
+    
+    xhr.open('GET','http://localhost:3000/getData?number='+ss); //6.
+    xhr.send(); //7.
+   
+});
