58c030815c0ca4ded7a172e924b087c50efe253a
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-03 00:27:26 +0800

    test the knowing user logic

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser= require('body-parser');
 const cookieParser = require('cookie-parser');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-const app = express();
-
+//Testing sql part
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -41,7 +41,8 @@ app.get('/createtable',(req,res)=>{
         res.send('User Table created!!');
     })
 })
-
+// End testing sql
+// Assignment 3 part
 app.get('/member', (req, res) => {
     res.cookie('cookiect',req.query.aname);
     console.log("cookie saved through get method from reaching /member")
@@ -52,12 +53,12 @@ app.get('/home', (req,res) => {
     if (req.cookies.cookiect) {
         console.log("嗨妳好，如果cookie的確存在，則我會出現");
         res.render('home',{customername: req.cookies.cookiect});
-
+        //store the cookie
     } else {
         console.log("Cookie 不存在. 如果是你還沒讓使用者填單，那沒關係，如果使用者已經填單了但還是看到我，那打掉重練!")
         res.render('home',{customername: req.cookies.cookiect});
         
-        //creates the query string on/trackName in URL
+        
     }
 })
 
