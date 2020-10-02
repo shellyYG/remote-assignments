fb06c814318c90769de9e3471df14b194868cf41
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-03 00:09:13 +0800

    install cookie parse, body parser, and pug

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
-const { isBuffer } = require('util');
+const bodyParser= require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
 
 const app = express();
 
@@ -36,7 +42,24 @@ app.get('/createtable',(req,res)=>{
     })
 })
 
+app.get('/member', (req, res) => {
+    res.cookie('cookiect',req.query.aname);
+    console.log("cookie saved through get method from reaching /member")
+    res.redirect('/home'); //redirect back to /myName
+})
 
+app.get('/home', (req,res) => {
+    if (req.cookies.cookiect) {
+        console.log("嗨妳好，如果cookie的確存在，則我會出現");
+        res.render('home',{customername: req.cookies.cookiect});
+
+    } else {
+        console.log("Cookie 不存在. 如果是你還沒讓使用者填單，那沒關係，如果使用者已經填單了但還是看到我，那打掉重練!")
+        res.render('home',{customername: req.cookies.cookiect});
+        
+        //creates the query string on/trackName in URL
+    }
+})
 
 app.listen('4300',()=>{
     console.log("Server started successfully on port 4300!")
