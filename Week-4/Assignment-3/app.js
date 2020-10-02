881f2875fd6a51bf764f183d573661f916289cfc
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-03 00:46:56 +0800

    successfully write email and pass into backend

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -44,19 +44,38 @@ app.get('/createtable',(req,res)=>{
 // End testing sql
 // Assignment 3 part
 app.get('/member', (req, res) => {
-    res.cookie('cookiect',req.query.aname);
+    res.cookie('cookieemail',req.query.emails);
+    res.cookie('cookiepass',req.query.passw);
     console.log("cookie saved through get method from reaching /member")
-    res.redirect('/home'); //redirect back to /myName
+    //store the name in the backend
+    let post = {email: req.query.emails ,password: req.query.passw};
+    let sql = 'INSERT INTO user SET?';
+    let query = db.query(sql, post, (err, result)=> {
+        if(err) throw err;
+        console.log(result);
+        res.redirect('/home'); //redirect back to /myName
+    })
 })
 
+
+app.get('/checktable',(req, res)=>{
+    let sql = 'SELECT * FROM user';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send(results);
+    });
+});
+
+
+
 app.get('/home', (req,res) => {
-    if (req.cookies.cookiect) {
+    if (req.cookies.cookieemail) {
         console.log("嗨妳好，如果cookie的確存在，則我會出現");
-        res.render('home',{customername: req.cookies.cookiect});
-        //store the cookie
+        res.render('home',{customername: req.cookies.cookieemail});
     } else {
         console.log("Cookie 不存在. 如果是你還沒讓使用者填單，那沒關係，如果使用者已經填單了但還是看到我，那打掉重練!")
-        res.render('home',{customername: req.cookies.cookiect});
+        res.render('home',{customername: req.cookies.cookieemail});
         
         
     }
