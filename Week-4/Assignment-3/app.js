a1083e3ac0b0dfaebabceadf7c37fdfbdc785983
shellyyangtw <chih-ching.yang@whu.edu>
2020-10-02 12:57:50 +0800

    create user table

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
+const { isBuffer } = require('util');
 
 const app = express();
-
-app.listen('4300',()=>{
-    console.log("Server started successfully on port 4300!")
-})
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password:'mysql2020'
+    password:'mysql2020',
+    database: 'assignment'
 });
 
 db.connect((err) => {
@@ -27,4 +25,19 @@ app.get('/createdb',(req,res)=> {
         console.log(result);
         res.send('Database created!');
     });
+})
+
+app.get('/createtable',(req,res)=>{
+    let sql = 'CREATE TABLE user(id int AUTO_INCREMENT, email VARCHAR(255), password VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send('User Table created!!');
+    })
+})
+
+
+
+app.listen('4300',()=>{
+    console.log("Server started successfully on port 4300!")
 })
